eb61441d87f57deb4d03ba6869c5f7583f0d0c0f
AdrianPerez28 <adrianp2889@gmail.com>
2022-01-24 13:00:36 -0500

    added nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './nav.css';
+import { Link } from "react-router-dom";
+
+const bar = ['Home', 'Us', 'Why', 'Donate', 'Sources'];
+
+
+function Nav() {
+  return <div>
+  
+  
+  </div>;
+}
+
+export default Nav;
