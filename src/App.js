223b4ad0fe06b5a4c29e5b3b414d6fa27cbb8d61
AdrianPerez28 <adrianp2889@gmail.com>
2022-01-25 15:13:58 -0500

    updated index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import Nav from './components/Nav'
+import {Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import RecyclingUS from './pages/RecyclingUS';
+import WhyRecycle from './pages/WhyRecycle';
+import Donate from './pages/Donate';
+import Source from './pages/Source';
+
 
 function App() {
   return (
     <div >
      <Nav/>
+     <Routes>
+       <Route path='/Home' element={<Home />} />
+       <Route path='/RecyclingUS' element={<RecyclingUS />} />
+       <Route path='/WhyRecycle' element={<WhyRecycle />} />
+       <Route path='/Donate' element={<Donate />} />
+       <Route path='/Source' element={<Source />} />
+     </Routes>
+
     </div>
   );
 }
