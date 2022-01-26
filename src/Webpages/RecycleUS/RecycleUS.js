02754340a309f54534c12d9a6f3d511777da1488
Home <home@macbook-pro.myfiosgateway.com>
2022-01-26 18:23:04 -0500

    recycleinusa

--- src/Webpages/RecycleUS/RecycleUS.js
+++ src/Webpages/RecycleUS/RecycleUS.js
@@ -1,9 +1,58 @@
+import './RecycleUS.css'
 import React from 'react'
+import Box from '@mui/material/Box';
+
+
+
+
 
 const RecycleUS = () => {
+
     return (
-        <div>
-            hi
+        <div className='why-recycle'>
+            <div className='big-fact'>
+              <Box sx={{width:'100%',alignItems:'center', justifyContent:'center'}}>
+                  <p style={{color:'white'}}>
+                      FACT: The U.S. offshores its plastic waste to other countries around the world instead of recycling
+                  </p>
+              </Box>
+                
+            </div>
+            <div className='short-message'>
+                <p style={{color:'#F1C24A'}}> The U.S. generates more plastic waste than any other nation and exporting it to other countries does not absolve us of its impact</p>
+            </div>
+            <div className='why-not-recycling'>
+                <Box sx={{alignItems:'center'}}>
+                <p style={{color:'#F1C24A'}}>
+                      Why is our plastic not being recycled?
+                </p>
+                  <text style={{color:'white'}}>Insert Reason Here</text>
+                </Box>
+            </div>
+            <div className='where-plastic-goes'>
+                <Box>
+                    <p style={{color:'#F1C24A'}}>Where Does Our plastic actually go?
+
+                    </p>
+                    <text style={{color:'white'}}>Insert Location Here</text>
+
+
+                </Box>
+            </div>
+            <div className='waste-production-usa'>
+                <p style={{color:'#F1C24A'}}> Average Waste Production in the USA</p>
+                 
+                <Box>
+                Number of Household Plastic Recorded in the US
+                </Box>
+                <Box>
+                Number of State Plastic Recorded in the US
+                </Box>
+                <Box>
+                Number of Country-wide Plastic Recorded in the US 
+                </Box>
+            </div>
+            
         </div>
     )
 }
