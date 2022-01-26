b96069b68d11ea6f74763725f214498cc467b878
AdrianPerez28 <adrianp2889@gmail.com>
2022-01-26 12:39:36 -0500

    added text and buttons to grids

--- src/Webpages/WhyRecycle/WhyRecycle.js
+++ src/Webpages/WhyRecycle/WhyRecycle.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import './WhyRecycle.css'
 import Grid from '@mui/material/Grid';
-
-
+import Button from '@mui/material/Button';
+import '../../App.css';
+ /* .title-text - classname for title for descriptive sections */
+/*.findbutton - classname for the find out how button */
+/*.whyrecycle-container - classname for parent div */
 const WhyRecycle = () => {
     return (
         <div className='whyrecycle-container' >
         <Grid container spacing={2}>
             <Grid item xs={6}>
-                1
+            <h2 className='title-text'>Recycling Keeps Trash Out of The Landfill & Prevents Pollution</h2>
+            <Button className='findbutton' variant='contained' >FIND OUT HOW</Button>
             </Grid>
             <Grid item xs={6}>
                 2
@@ -17,10 +20,12 @@ const WhyRecycle = () => {
                 3
             </Grid>
             <Grid item xs={6}>
-                4
+                <h2 className='title-text'>Recycling Supports Public Health & Creates Jobs</h2>
+                <Button className='findbutton' variant='contained' >FIND OUT HOW</Button>
             </Grid>
             <Grid item xs={6}>
-                5
+                <h2 className='title-text'>Recycling Conserves Energy & Saves Resources</h2>
+                <Button className='findbutton' variant='contained' >FIND OUT HOW</Button>
             </Grid>
             <Grid item xs={6}>
                 6
