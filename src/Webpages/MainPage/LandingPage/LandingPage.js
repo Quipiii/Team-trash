efb2923562027c55b3d55c793ca093bf70de139b
Jaleel Bennett <>
2022-01-27 10:29:15 -0500

    landing page final

--- src/Webpages/MainPage/LandingPage/LandingPage.js
+++ src/Webpages/MainPage/LandingPage/LandingPage.js
@@ -7,12 +7,12 @@ const LandingPage = () => {
     return (
         <section className='landing-page'>
             <div className='landing-page-text-wrap'>
-                <div className='fade-in' style={{transitionDelay: '100ms'}}><h1>Hey Yall, </h1></div>
+                <div className='fade-in' style={{transitionDelay: '100ms', fontSize: 'clamp(var(--fz-sm),5vw,var(--fz-md))'}}><h1>Hey Yall, </h1></div>
                 <div style={{transitionDelay: '200ms'}}><h2 className='big-heading'>Let's start recycling</h2></div>
                 <div style={{transitionDelay: '300ms'}}><h2 className='big-heading'>today so we can have </h2></div>
                 <div style={{transitionDelay: '400ms'}}><h2 className='big-heading'>a better future tomorrow.</h2></div>
                 <div style={{transitionDelay: '500ms'}}>
-                    <Button variant='outlined' sx={{margin: '30px 0px 0px 4px', color: 'var(--section-title-color)', borderColor: 'var(--section-title-color)'}}>
+                    <Button className='image-border' variant='outlined' sx={{margin: '30px 0px 0px 4px', color: 'var(--section-title-color)', borderColor: 'var(--section-title-color)'}}>
                         <Link href='#mission-statement' underline='none' sx={{color: 'var(--section-title-color)'}}>Click to get sarted</Link>
                     </Button>
                 </div>
